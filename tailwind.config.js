bb1ac4e97ee4cccaa5cdce2d590bd0fcbfeb97c0
Abdullah Bin Ahmed <ronaldinho1415@gmail.com>
2025-08-31 18:15:39 +0000

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,14 @@
-/** @type {import('tailwindcss').Config} */
-export default {
+// tailwind.config.js
+module.exports = {
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,jsx,ts,tsx}",
+    // Explicitly add directories if needed:
+    "./src/components/**/*.{js,jsx,ts,tsx}",
+    "./src/features/**/*.{js,jsx,ts,tsx}",
+    "./src/services/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
