865950dfea221c83ded99235c30d1f2fb81f2bbd
Abdullah Bin Ahmed <ronaldinho1415@gmail.com>
2025-08-31 18:15:39 +0000

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,11 @@
 // src/main.jsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import AppWrapper from './App.jsx';
-
-// FIXED: This line imports all your Tailwind styles
-import './index.css';
+import App from './App.jsx';
+import './index.css'; // ← Critical line
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <AppWrapper />
-  </React.StrictMode>,
+    <App />
+  </React.StrictMode>
 );
-
