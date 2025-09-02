6fcfdbbc00ad671cdadddd6671014085a06dff48
Abdullah Bin Ahmed <ronaldinho1415@gmail.com>
2025-09-02 08:41:33 +0000

    update

---
+++ postcss.config.mjs
@@ -0,0 +1,7 @@
+export default {
+  plugins: {
+    '@tailwindcss/postcss': {},
+    // autoprefixer is often added for browser compatibility, but the core fix is above
+    autoprefixer: {},
+  },
+}
