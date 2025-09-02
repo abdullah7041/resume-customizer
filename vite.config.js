059c2923bf723f897eb22a2ea1077011925bdb0c
Abdullah Bin Ahmed <ronaldinho1415@gmail.com>
2025-09-02 08:07:18 +0000

    Update

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // FIXED: Removed the alias configuration to enforce consistent relative paths.
 })
