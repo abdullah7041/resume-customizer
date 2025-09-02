e718d9630a30fb4a7b451e5f53e4b5cb533d9088
Abdullah Bin Ahmed <ronaldinho1415@gmail.com>
2025-09-02 17:10:44 +0000

    Update to tailwind v4

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'   // if not already installed: npm i -D @vitejs/plugin-react
-import tailwindcss from '@tailwindcss/vite'
-
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-})
