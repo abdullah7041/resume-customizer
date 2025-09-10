d365ff7dbf7dee48c2bbbbdc4da4c45539c09c9e
Abdullah Bin Ahmed <ronaldinho1415@gmail.com>
2025-09-10 17:08:21 +0300

    codex: record test plan\n\nTest Plan: npm run lint && npm run test (fails: Missing script) && npm run build

---
+++ eslint.config.js
@@ -0,0 +1,33 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+
+export default [
+  {
+    ignores: ['node_modules'],
+  },
+  js.configs.recommended,
+  {
+    files: ['**/*.{js,jsx,ts,tsx}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      sourceType: 'module',
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+      parserOptions: {
+        ecmaFeatures: { jsx: true },
+      },
+    },
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
+    rules: {
+      'react-hooks/rules-of-hooks': 'error',
+      'react-refresh/only-export-components': 'warn',
+    },
+  },
+]
