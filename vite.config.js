aef1246f4a7dc5d7e515509f7fbfb343821db3e3
Abdullah Bin Ahmed <ronaldinho1415@gmail.com>
2025-09-12 17:39:14 +0000

    Fix bugs

---
+++ vite.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    environment: "happy-dom",        // <-- switch from jsdom
+    globals: true,
+    setupFiles: "./src/test/setup.ts",
+    css: true
+  },
+});
