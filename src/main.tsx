1a16bb3def9fa7bdc8efbd63c33473fb4701fbdf
Abdullah Bin Ahmed <ronaldinho1415@gmail.com>
2025-09-20 22:54:52 +0000

    fix: remove local preload, dynamic preload skyline via Supabase

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,14 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import { AuthProvider } from "./hooks/useAuth";
 import "./index.css";
+import { skyline } from "./lib/assets";
+
+// Preload skyline image at bootstrap
+const preloadLink = document.createElement("link");
+preloadLink.rel = "preload";
+preloadLink.as = "image";
+preloadLink.href = skyline();
+document.head.appendChild(preloadLink);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
@@ -14,5 +22,3 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     </BrowserRouter>
   </React.StrictMode>
 );
-
-export default App;
