177a98b384ffb0e7ca47517c563396c2732a5b9a
Abdullah Bin Ahmed <ronaldinho1415@gmail.com>
2025-09-25 22:02:43 +0300

    feat: add persistent theme toggle (#53)

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,10 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import { AuthProvider } from "./hooks/useAuth";
+import { initializeTheme } from "./hooks/useTheme";
 import "./index.css";
+
+initializeTheme();
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
